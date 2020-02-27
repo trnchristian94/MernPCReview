416501456e1f058eb10315a98fb618d3cbc94ebf
Christian <trnchristian94@gmail.com>
2020-02-27 16:18:58 +0100

    Added autoredirect when user logs out. Autoredireccionamiento si el usuario cierra sesión.

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -1,135 +1,136 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import { connect } from "react-redux";
 import { logoutUser } from "userLogic/actions/authActions";
 import { loginUser } from "userLogic/actions/authActions";
 
-import { Link } from "react-router-dom";
+import { useToasts } from "react-toast-notifications";
+
+import { Link, Redirect } from "react-router-dom";
 
 import { Navbar, Nav, Form, Button } from "react-bootstrap";
 
-type MyProps = {
+interface Props {
   auth: any;
   loginUser: any;
   logoutUser: any;
   errors: any;
-};
-type MyState = {
-  email: string;
-  password: string;
-  [x: number]: any;
-};
-class Header extends Component<MyProps, MyState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      email: "",
-      password: ""
-    };
-  }
+}
 
-  onLogoutClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
-    this.props.logoutUser();
-  };
+function Header({ auth, loginUser, logoutUser, errors }: Props) {
+  const { addToast } = useToasts();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [connected, setConnected] = useState(false);
 
-  onChange = (e: any) => {
-    this.setState({ [e.target.id]: e.target.value });
-  };
-  UNSAFE_componentWillReceiveProps(nextProps: any) {
+  /*UNSAFE_componentWillReceiveProps(nextProps: any) {
     if (nextProps.auth.isAuthenticated) {
       window.location.href = "./dashboard";
     }
-  }
+  }*/
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      setConnected(true);
+    }
+  }, [auth.isAuthenticated]);
 
-  onSubmit = (e: any) => {
+  const onLogoutClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
-    const userData = {
-      email: this.state.email,
-      password: this.state.password
-    };
-    this.props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
+    logoutUser();
+    setConnected(false);
   };
 
-  render() {
-    console.log(window.location);
-    const { user } = this.props.auth;
-    return (
-      <>
-        <Navbar id="pc-principalNavbar">
-          <Nav>
-            <Link to={"/"} className="nav-link">
-              <Navbar.Brand id="pc-principalBrand">The PC Review</Navbar.Brand>
-            </Link>
-          </Nav>
-        </Navbar>
-        <Navbar variant="dark">
-          <Nav className="mr-auto">
-            <Link to={"/"} className="nav-link">
-              Home
-            </Link>
-            <Link to={"/tasks"} className="nav-link">
-              Tasks
-            </Link>
-            {!this.props.auth.isAuthenticated ? (
-              <>
-                <Link to={"/register"} className="nav-link">
-                  Register
-                </Link>
-                <Link to={"/login"} className="nav-link">
-                  Login
-                </Link>
-              </>
-            ) : (
-              <Link to={"/userList"} className="nav-link">
-                Users
-              </Link>
-            )}
-          </Nav>
-          {this.props.auth.isAuthenticated ? (
-            <div>
-              <span className="mr-2">Welcome {user.name.split(" ")[0]}!</span>
-              <Button variant="dark" size="sm" onClick={this.onLogoutClick}>
-                Logout
-              </Button>
-            </div>
-          ) : (
-            <Form inline onSubmit={this.onSubmit}>
-              <Form.Row>
-                <Form.Group className="mr-2">
-                  <Form.Label className="mr-2">Email</Form.Label>
-                  <Form.Control
-                    size="sm"
-                    type="email"
-                    placeholder="Enter email"
-                    onChange={this.onChange}
-                    value={this.state.email}
-                    id="email"
-                  />
-                </Form.Group>
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    const userData = {
+      email: email,
+      password: password
+    };
+    loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
+  };
 
-                <Form.Group className="mr-2">
-                  <Form.Label className="mr-2">Password</Form.Label>
-                  <Form.Control
-                    size="sm"
-                    type="password"
-                    placeholder="Password"
-                    onChange={this.onChange}
-                    value={this.state.password}
-                    id="password"
-                  />
-                </Form.Group>
-                <Button variant="dark" type="submit" size="sm">
-                  Login
-                </Button>
-              </Form.Row>
-            </Form>
+  const { user } = auth;
+  return (
+    <>
+      {!connected && <Redirect push to="/login" />}
+      {connected && <Redirect push to="/dashboard" />}
+      <Navbar id="pc-principalNavbar">
+        <Nav>
+          <Link to={"/"} className="nav-link">
+            <Navbar.Brand id="pc-principalBrand">The PC Review</Navbar.Brand>
+          </Link>
+        </Nav>
+      </Navbar>
+      <Navbar variant="dark">
+        <Nav className="mr-auto">
+          <Link to={"/"} className="nav-link">
+            Home
+          </Link>
+          <Link to={"/tasks"} className="nav-link">
+            Tasks
+          </Link>
+          {!auth.isAuthenticated ? (
+            <>
+              <Link to={"/register"} className="nav-link">
+                Register
+              </Link>
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            </>
+          ) : (
+            <Link to={"/userList"} className="nav-link">
+              Users
+            </Link>
           )}
-        </Navbar>
-      </>
-    );
-  }
+        </Nav>
+        {auth.isAuthenticated ? (
+          <div>
+            <span className="mr-2">Welcome {user.name.split(" ")[0]}!</span>
+
+            <Button variant="dark" size="sm" onClick={onLogoutClick}>
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Form inline onSubmit={onSubmit}>
+            <Form.Row>
+              <Form.Group className="mr-2">
+                <Form.Label className="mr-2">Email</Form.Label>
+                <Form.Control
+                  size="sm"
+                  type="email"
+                  placeholder="Enter email"
+                  onChange={(e: any) => setEmail(e.target.value)}
+                  value={email}
+                  id="email"
+                />
+              </Form.Group>
+
+              <Form.Group className="mr-2">
+                <Form.Label className="mr-2">Password</Form.Label>
+                <Form.Control
+                  size="sm"
+                  type="password"
+                  placeholder="Password"
+                  onChange={(e: any) => setPassword(e.target.value)}
+                  value={password}
+                  id="password"
+                />
+              </Form.Group>
+              <Button variant="dark" type="submit" size="sm">
+                Login
+              </Button>
+            </Form.Row>
+          </Form>
+        )}
+      </Navbar>
+    </>
+  );
 }
+
 const mapStateToProps = (state: any) => ({
   auth: state.auth,
   errors: state.errors
